675672917bc5de4a60f4c54fab1e57ae7eb33a49
pamu47 <pamudithamaduranga@gmail.com>
2019-03-23 12:31:01 +0530

    Http server initialized

--- Lab 4/app.js
+++ Lab 4/app.js
@@ -1,15 +1,8 @@
 const os = require('os')
 const fs = require('fs')
+const http = require('http')
 const fileName = './test.txt'
 const outFileName = './test-copy.txt'
-
-//Copy the context of a file to another using Streams
-const readStream = fs.createReadStream(fileName)
-const writeStream = fs.createWriteStream(outFileName)
-
-readStream.pipe(writeStream)
-
-
 
 //...Obtain system overview...
 //console.log('Architecture :' +os.arch())
@@ -24,6 +17,20 @@ fs.readFile(fileName, (err,data) => {
     console.log(data.toString())
 })
 
+//Copy the context of a file to another using Streams
+const readStream = fs.createReadStream(fileName)
+const writeStream = fs.createWriteStream(outFileName)
 
+readStream.pipe(writeStream)
 
+//listening to data of readStream and printing
+readStream.on('data',data => {
+    console.log(data.toString())
+})
 
+//creating the http server
+http.createServer((req,res) => {
+    res.setHeader('Content-type','text/html')
+    res.write('<h1>Hello World</h1>')
+    res.end();
+}).listen(3000);
